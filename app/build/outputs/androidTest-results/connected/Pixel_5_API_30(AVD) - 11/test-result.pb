fba211bb0047e7eb30054c976b1fdab1b6ed72e1
Sathira443 <liyanapathiranasathira@gmail.com>
2022-05-11 00:10:23 +0530

    workflow added

---
+++ app/build/outputs/androidTest-results/connected/Pixel_5_API_30(AVD) - 11/test-result.pb
@@ -0,0 +1,20 @@
+
+Ά
+d
+ApplicationTest&lk.ac.mrt.cse.dbs.simpleexpensemanagertestAddAccount2’ήκ“ΐÒZ:’ήκ“€ϊΆ"Ϊ
+
+logcatandroidΔ
+ΑD:\GitHub Projects\SimpleExpenseManager\app\build\outputs\androidTest-results\connected\Pixel_5_API_30(AVD) - 11\logcat-lk.ac.mrt.cse.dbs.simpleexpensemanager.ApplicationTest-testAddAccount.txt"
+
+device-infoandroid
+D:\GitHub Projects\SimpleExpenseManager\app\build\outputs\androidTest-results\connected\Pixel_5_API_30(AVD) - 11\device-info.pb"
+
+device-info.meminfoandroidz
+xD:\GitHub Projects\SimpleExpenseManager\app\build\outputs\androidTest-results\connected\Pixel_5_API_30(AVD) - 11\meminfo"
+
+device-info.cpuinfoandroidz
+xD:\GitHub Projects\SimpleExpenseManager\app\build\outputs\androidTest-results\connected\Pixel_5_API_30(AVD) - 11\cpuinfo*‚
+c
+test-results.logOcom.google.testing.platform.runtime.android.driver.AndroidInstrumentationDriver
+‰D:\GitHub Projects\SimpleExpenseManager\app\build\outputs\androidTest-results\connected\Pixel_5_API_30(AVD) - 11\testlog\test-results.log 2
+text/plain
--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -16,14 +16,39 @@
 
 package lk.ac.mrt.cse.dbs.simpleexpensemanager;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.ExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.PersistentExpenseManager;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest{
+    private static ExpenseManager expenseManager;
+
+    //====================
+    @Before
+    public void setup(){
+        Context context = ApplicationProvider.getApplicationContext();
+        expenseManager = new PersistentExpenseManager(context);
     }
+
+    @Test
+    public void testAddAccount(){
+        expenseManager.addAccount("123ABC", "BoC", "Ranjani", 10000);
+        List<String> accountNumbers = expenseManager.getAccountNumbersList();
+        assertTrue(accountNumbers.contains("123ABC"));
+
+    }
+
 }
